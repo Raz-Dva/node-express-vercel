76f749f92bd21aa8b0486b4d50127277b0472c87
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2023-01-29 16:57:24 +0100

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const companyInfo = require("./routes/info-route");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 // Routes
 app.use('/info', companyInfo);
